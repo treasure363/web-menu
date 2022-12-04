4da3ca47f4f8716fee83b7935c68cb4b02fd638d
Ankit Choudhury <ankit.choudhury0608@gmail.com>
2022-12-05 02:48:05 +0530

    integrated localhost

--- web_menu/settings.py
+++ web_menu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pxi$=%!iw_uxx*lqbu2e@qb8lskdz@q7=$3a#p=+l9dw5qu=qp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
