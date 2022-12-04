494c544ef68df4da1166a8bfd59812e1434fd3b7
Ankit Choudhury <ankit.choudhury0608@gmail.com>
2022-12-05 02:20:28 +0530

    added dummy data

--- menu/views.py
+++ menu/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Category
 
 # Create your views here.
 
 def index(request):
-    pass
+    return redirect(request.path + 'menu/')
 
 def menu(request):
-    pass
+    user = request.user 
+    categories = list(Category.objects.all())
+    return render(request, "menu/index.html", {
+        "categories" : categories
+    })
 
-def category(request):
-    pass
+def category(request, category):
+    return render(request, "menu/category.html", {
+        "items":list(Category.objects.get(name=category).category.all()),
+        "header": category
+    })
 
 def item(request):
     pass
