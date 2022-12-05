7a5d3ff763be9b3b6365da93247be8981d31c8f9
Ankit Choudhury <ankit.choudhury0608@gmail.com>
2022-12-05 07:59:07 +0530

    added cart view and login

--- menu/urls.py
+++ menu/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path("menu/<str:category>", views.category, name = "category"),
     path("menu/<str:category>/<str:item>", views.item, name = "item"),
     path("cart/", views.cart, name="cart"),
+    path("cart/all/", views.cart_all, name="cartall"),
     path("cart/add/<int:item_id>/<int:quantity>", views.add_to_cart, name="add"),
-    # path("order/", views.order, name = "order"),
-    # path("login/", views.login, name = "login"),
+    path("login/", views.login_view, name = "login"),
+    path("logout/", views.logout_view, name = "logout"),
+    path("register", views.register, name="register"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+from sqlite3 import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
+from django.contrib.auth.models import User
 from .models import Category, Item, Cart
 
 # Create your views here.
@@ -38,6 +42,19 @@ def add_to_cart(request, item_id, quantity):
         cart.save()
     return HttpResponse()
 
+def del_from_cart(request, item_id, quantity):
+    if quantity==0:
+        return HttpResponse()
+    item = Item.objects.get(pk=item_id)
+    if Cart.objects.filter(item_id=item).exists():
+        cart = Cart.objects.get(item_id=item)
+        cart.quantity = quantity
+        cart.save()
+    else:
+        cart = Cart(table = request.user, item_id=item, quantity=quantity)
+        cart.save()
+    return HttpResponse()
+
 def cart(request):
     cart = Cart.objects.filter(table=request.user)
     items = []
@@ -49,3 +66,67 @@ def cart(request):
         "quantity": True
     })
 
+def cart_all(request):
+    cart = Cart.objects.all()
+    items = []
+    for c in cart:
+        items.append(c.item_id)
+    return render(request, "menu/category.html", {
+        "items":items,
+        "header": "My Cart",
+        "quantity": True
+    })
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "menu/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        if request.user.is_authenticated:
+            return redirect('index')
+        return render(request, "menu/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "menu/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "menu/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        if request.user.is_authenticated:
+            return redirect('index')
+        return render(request, "menu/register.html")
